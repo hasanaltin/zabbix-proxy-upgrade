61ff9a086bb58c9ea68336529296196713e3dc27
hasanaltin <62545852+hasanaltin@users.noreply.github.com>
2022-06-08 10:40:45 +0200

    Update zabbix-proxy-upgrade.sh

--- zabbix-proxy-upgrade.sh
+++ zabbix-proxy-upgrade.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+INSTALL_CMD="yum -y upgrade"
+
 
 systemctl stop zabbix-proxy
 rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/8/x86_64/zabbix-release-6.0-1.el8.noarch.rpm
 dnf clean all 
 yum makecache
-yum upgrade zabbix-proxy-mysql -y
-yum install zabbix-apache-conf -y 
+$INSTALL_CMD zabbix-proxy-mysql
+$INSTALL_CMD zabbix-apache-conf
 systemctl start zabbix-proxy
 zabbix_proxy --version
